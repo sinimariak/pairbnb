917c7ca6d6f86e7606266abc55fcc5a32bbad193
Sini Jaakkola <sini.k.jaakkola@gmail.com>
2016-04-05 16:03:23 +0800

    completed facebook signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < Clearance::SessionsController
 
       def create_from_omniauth
-        byebug
         auth_hash = request.env["omniauth.auth"]
 
         authentication = Authentication.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) || Authentication.create_with_omniauth(auth_hash)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ActiveRecord::Base
 	has_many :authentications, :dependent => :destroy
   include Clearance::User
-
-  include Clearance::User
-
+ 
   def self.create_with_auth_and_hash(authentication,auth_hash)
     create! do |u|
       u.first_name = auth_hash["info"]["first_name"]
       u.email = auth_hash["extra"]["raw_info"]["email"]
+      u.password = SecureRandom.hex(4)
       u.authentications<<(authentication)
     end
   end
